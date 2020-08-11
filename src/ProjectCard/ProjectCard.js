f1022c58bba2836d64ea4070bb46c09d65455999
Alexandru-Valentin Harapu <aharapu2@yahoo.com>
2020-08-11 08:08:33 +0300

    add semantic-ui change SectionMain.js from function to class

--- src/ProjectCard/ProjectCard.js
+++ src/ProjectCard/ProjectCard.js
@@ -15,7 +15,47 @@ const ProjectCard = (props) => {
 					<li key={index}>{item}</li>
 				))}
 			</ul>
-			<img alt="project preview" src={gifUrl} height='100px' width='100px' />
+			<img alt='project preview' src={gifUrl} height='100px' width='100px' />
+			<div className='ui card'>
+				<div className='content'>
+					<img
+                        alt="wtf"
+						className='right floated mini ui image'
+						src='/images/avatar/large/elliot.jpg'
+					/>
+					<div className='header'>Elliot Fu</div>
+					<div className='meta'>Friends of Veronika</div>
+					<div className='description'>
+						Elliot requested permission to view your contact details
+					</div>
+				</div>
+				<div className='extra content'>
+					<div className='ui two buttons'>
+						<div className='ui basic green button'>Approve</div>
+						<div className='ui basic red button'>Decline</div>
+					</div>
+				</div>
+			</div>
+			<div className='item'>
+				<div className='image'>
+					<img alt="wtf" src={gifUrl} />
+				</div>
+				<div className='content'>
+					<a href="www.wtf.io" className='header'>Watchmen</a>
+					<div className='meta'>
+						<span className='cinema'>IFC</span>
+					</div>
+					<div className='description'>
+						<p></p>
+					</div>
+					<div className='extra'>
+						<div className='ui right floated primary button'>
+							Buy tickets
+							<i className='right chevron icon'></i>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/SectionMain/SectionMain.js
+++ src/SectionMain/SectionMain.js
@@ -1,39 +1,41 @@
-import React, { useState } from 'react';
-import ProjectCard from '../ProjectCard/ProjectCard'
+import React from 'react';
+import ProjectCard from '../ProjectCard/ProjectCard';
 import './SectionMain.css';
+export class SectionMain extends React.Component {
+	state = { projectData: null, err: null };
 
-const SectionMain = () => {
-	const [isLoading, setIsLoading] = useState(true);
-	const [projectData, setProjectData] = useState(null);
-
-	if (isLoading) {
+	componentDidMount() {
 		const contentful = require('contentful');
 		const client = contentful.createClient({
-			// This is the space ID. A space is like a project folder in Contentful terms
 			space: 'al1ikk93hxev',
-			// This is the access token for this space. Normally you get both ID and the token in the Contentful web app
 			accessToken: 'MGR6ZpY44sfKOtgk33ias_3tZmj0yip5rv2zJfA0kHw',
 		});
-		// This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
 		client
 			.getEntries({
 				content_type: 'projectCard',
 			})
 			.then((entries) => {
-				setProjectData(entries);
-				setIsLoading(false);
+				this.setState({ projectData: entries });
 			})
-			.catch((err) => console.log(err));
-
-		return <div>wait for it...</div>;
+            .catch((err) => this.setState({err: err}));
 	}
 
-
-	return (
-		<div className='section-main'>
-            {projectData.items.map(project => <ProjectCard key={project.sys.id} cardData={project}/>)}
-		</div>
-	);
-};
+	render() {
+		if (!this.state.projectData && !this.state.err) {
+			return <div>loading...</div>;
+        }
+        if (this.state.err) {
+            console.log('loading error', this.state.err);
+            return <div>huston, we have a problem</div>
+        }
+		return (
+			<div className='section-main'>
+				{this.state.projectData.items.map((project) => (
+					<ProjectCard key={project.sys.id} cardData={project} />
+				))}
+			</div>
+		);
+	}
+}
 
 export default SectionMain;
