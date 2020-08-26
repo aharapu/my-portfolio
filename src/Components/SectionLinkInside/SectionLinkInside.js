c1fd3f0fe9f86d6f560d2968afa0fd5015158f7d
Alexandru-Valentin Harapu <aharapu2@yahoo.com>
2020-08-26 14:32:08 +0300

    projects navigation - in development

--- src/Components/SectionLinkInside/SectionLinkInside.js
+++ src/Components/SectionLinkInside/SectionLinkInside.js
@@ -1,27 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
 import { Stagger, Fade } from 'react-animation-components';
+import { projectDataState } from '../../App'
 
 
 const SectionLinkInside = () => {
+	const projectData = useRecoilValue(projectDataState);
+
 	return (
-		<Stagger in delay={130} duration={1000} className='section-link-inside'>
-			<Fade in>
-				<Link to='/about'>
-					<div className='link-inside'>about me</div>
-				</Link>
-			</Fade>
-			<Fade in>
-				<Link to='/projects'>
-					<div className='link-inside'>projects</div>
-				</Link>
-			</Fade>
-			<Fade in>
-				<Link to='/resume'>
-					<div className='link-inside'>résumé</div>
-				</Link>
-			</Fade>
-		</Stagger>
+		<div className='section-link-inside'>
+			<div className='container-link-project'>
+				{projectData ? <Link className='link-project' to={`/projects/#${projectData.items[2].sys.id}`}>go to project</Link> : null}
+				{projectData ? (<Link to={{ pathname: "/projects", search: "", hash: `#${projectData.items[2].sys.id}`, state: { fromSideLink: true } }} >go with obj</Link> ) : null}
+
+				<a className='link-project' href='#somewhere'>
+					i stay on the right	
+				</a>
+				<a className='link-project' href='#somewhere'>
+					and another project
+				</a>
+			</div>
+			<Stagger in delay={130} duration={1000} className='container-link-inside'>
+				<Fade in>
+					<Link to='/about'>
+						<div className='link-inside'>about me</div>
+					</Link>
+				</Fade>
+				<Fade in>
+					<Link to='/projects'>
+						<div className='link-inside'>projects</div>
+					</Link>
+				</Fade>
+				<Fade in>
+					<Link to='/resume'>
+						<div className='link-inside'>résumé</div>
+					</Link>
+				</Fade>
+			</Stagger>
+		</div>
 	);
 };
 
