d512e5a833a686694185452a0c2485034ca89a13
Alexandru-Valentin Harapu <aharapu2@yahoo.com>
2020-08-26 08:28:33 +0300

    emailJS form

--- src/Components/AboutContainer/AboutContainer.js
+++ src/Components/AboutContainer/AboutContainer.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
+import emailjs from 'emailjs-com';
 
 const AboutContainer = () => {
-    return (
-        <div>
-            about me
-        </div>
-    )
-}
+	const sendEmail = e => {
+		e.preventDefault();
 
-export default AboutContainer
+		emailjs
+			.sendForm('gmail', 'template_RzMhP9ra', e.target, process.env.REACT_APP_EMAILJS_USERID)
+			.then(
+				result => {
+					console.log(result.text);
+				},
+				error => {
+					console.log(error.text);
+				},
+			);
+	};
+	return (
+		<div>
+			<form className='contact-form' onSubmit={sendEmail}>
+				<input type='hidden' name='to_name' value='vasilescu' />
+				<label>Name</label>
+				<input type='text' name='from_name' />
+				<label>Email</label>
+				<input type='email' name='reply_to' />
+				<label>Message</label>
+				<textarea name='message_html' />
+				<input type='submit' value='Send' />
+			</form>
+		</div>
+	);
+};
+
+export default AboutContainer;
