9e71e66da0f9984cc3fd62b4ed8fc7128738774c
Alexandru-Valentin Harapu <aharapu2@yahoo.com>
2020-10-01 12:06:05 +0300

    convert section left icons to styled components

--- src/Components/LinkOutside/LinkOutside.js
+++ src/Components/LinkOutside/LinkOutside.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import faker from 'faker';
-export class LinkOutside extends React.PureComponent {
+import icons from './styledIcons';
+
+class LinkOutside extends React.PureComponent {
 	render() {
+		console.log('this.props: ', this.props);
 		const { name, icon, linkSrc } = { ...this.props.info };
 		return (
 			<a className='link-outside' href={linkSrc} target='_blank' rel='noopener noreferrer'>
 				<h5>{name}</h5>
-				<img src={icon.fields.file.url} alt={name}></img>
+				{React.cloneElement(icons[name], { color: '#f6989a', className: 'styledIcon' })}
 			</a>
 		);
 	}
